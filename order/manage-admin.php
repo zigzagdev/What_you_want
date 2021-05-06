7aeb3fa375de181a0847efb7fefe08c32b37b3c3
Masatoshi <msts.oo0131@gmail.com>
2021-05-06 14:06:41 +0900

    manage-admin_button_create

--- order/manage-admin.php
+++ order/manage-admin.php
@@ -4,6 +4,49 @@
 <div class="main">
     <div class="wrapper">
         <h1>Manage Admin</h1>
+        <br/>
+       <!---Button--->
+       <a href="#" class="btn-primary">Add Admin</a>
+        <br/>
+        <br/>
+        <br/>
+       <table class="tbl-full">
+        <tr>
+          <th>M.O</th>
+          <th>Full Name</th>
+           <th>User Name</th>
+          <th>Actions</th>
+        </tr>
+
+        <tr>
+         <td>1.</td>
+         <td>Matthew</td>
+         <td>Matt</td>
+         <td>
+          <a href="#" class="btn-secondary">Update Admin</a>
+          <a href="#" class="btn-danger">Delete Admin</a>
+         </td>
+        </tr>
+        <tr>
+         <td>2.</td>
+         <td>Matthew</td>
+         <td>Matt</td>
+          <td>
+           <a href="#" class="btn-secondary">Update Admin</a>
+           <a href="#" class="btn-danger">Delete Admin</a>
+          </td>
+        </tr>
+        <tr>
+         <td>3.</td>
+         <td>Matthew</td>
+         <td>Matt</td>
+        <td>
+         <a href="#" class="btn-secondary">Update Admin</a>
+         <a href="#" class="btn-danger">Delete Admin</a>
+        </td>
+        </tr>
+
+       </table>
     </div>
 </div>
 <!--Main Section -->
