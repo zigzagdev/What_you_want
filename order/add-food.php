ba081931fe6ff6302a4002656275f60e58d27491
Masatoshi <msts.oo0131@gmail.com>
2021-05-12 14:52:35 +0900

    except_update_smoothlly_everything_is_ok

--- order/add-food.php
+++ order/add-food.php
@@ -30,9 +30,68 @@
      </td>
     </tr>
 
+    <tr>
+     <td>Select Image:</td>
+     <td>
+      <input type="file" name="image">
+     </td>
+    </tr>
+
+    <tr>
+     <td>Category:</td>
+     <td>
+      <select name="category">
+        <?php
+          $sql="SELECT * FROM tbl_category WHERE active='Yes'";
+          $rec=mysqli_query($connect,$sql);
+          $count=mysqli_num_rows($rec);
+         if($count>0)
+          {
+            while($row=mysqli_fetch_assoc($rec))
+             {
+               /*/ get category data /*/
+               $id=$row['id'];
+               $title=$row['title'];
+               ?>
+              <option value="<?php echo $id;?>"><?php echo $title; ?></option>
+               <?php
+             }
+          }
+         else
+          {
+            ?>
+              <option value="0">No Category Found</option>
+           <?php
+          }
+        ?>
+          <option value="1">Food</option>
+          <option value="2">Snacks</option>
+      </select>
+     </td>
+    </tr>
+
+    <tr>
+     <td>Featured:</td>
+     <td>
+      <input type="radio" name="featured" value="Yes"> Yes
+      <input type="radio" name="featured" value="No"> No
+     </td>
+    </tr>
+    <tr>
+     <td>Active:</td>
+     <td>
+      <input type="radio" name="active" value="Yes"> Yes
+      <input type="radio" name="active" value="No"> No
+     </td>
+    </tr>
+
+    <tr>
+     <td colspan="2">
+      <input type="submit" name="submit" value="Add food" class="btn-secondary">
+     </td>
+    </tr>
 
    </table>
-
   </form>
 
  </div>
